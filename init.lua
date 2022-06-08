6352ed8cb4d2bc924509c31058648278dbfbdb54
Lukas Olson <lukas@alumni.ubc.ca>
2022-06-08 11:26:01 -0700

    Fixed out of memory issues (and added checkpoints for when game does crash)

--- init.lua
+++ init.lua
@@ -1,7 +1,17 @@
-UBCMap = { path = minetest.get_modpath("ubc_campus_minetest_map") }
+UBCMap = { path = minetest.get_modpath("ubc_campus_minetest_map"), storage = minetest.get_mod_storage() }
 
-function UBCMap.placeSchematic(pos1, pos2, schematic)
+function UBCMap:placeSchematic(pos1, pos2, schematic, rotation, replacement, force_placement)
 
+    -- Read data into LVM
+    local vm = minetest.get_voxel_manip()
+    local emin, emax = vm:read_from_map(pos1, pos2)
+    local a = VoxelArea:new {
+        MinEdge = emin,
+        MaxEdge = emax
+    }
+
+    minetest.place_schematic_on_vmanip(vm, pos1, schematic, rotation, replacement, force_placement)
+    vm:write_to_map(true)
 end
 
 function UBCMap.place(startPosition)
@@ -12,6 +22,7 @@ function UBCMap.place(startPosition)
     end
     -- manually define tile numbers because searching files on disk with lua is stupid
     local tiles = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28 }
+
     local coords = {}
     for xx = 0, 2500, 500 do
         for yy = 1, 7 do
@@ -19,32 +30,53 @@ function UBCMap.place(startPosition)
             table.insert(coords, { xx + startPosition.x, y + startPosition.z })
         end
     end
-    for _, v in pairs(tiles) do
+
+    -- If we crash during world generation, we'll try again.
+    local startingValue = UBCMap.storage:get_int("placementProgress")
+    if (startingValue == nil or startingValue == 0) then
+        startingValue = 1
+        minetest.chat_send_all("Placing UBC Map")
+    else
+        minetest.chat_send_all("Previous minetest instance crashed while placing UBC Map... Trying again from position: " .. startingValue)
+    end
+
+    for v = startingValue, 28 do
+        --for _, v in pairs(tiles) do
         coord = coords[v]
         -- y must always be -3 because the schematic pos starts here
         -- this maintains the true elevations across the map
         -- place terrain first
 
-        minetest.place_schematic({ x = coord[1], y = startPosition.y, z = coord[2] }, UBCMap.path .. "/schems/ubc_blocks_" .. tostring(v) .. ".mts", "0", nil, false)
-        minetest.chat_send_all("Placed terrain tile " .. tostring(v))
-        -- place trees second
-        minetest.place_schematic({ x = coord[1], y = startPosition.y, z = coord[2] }, UBCMap.path .. "/schems/ubc_trees_" .. tostring(v) .. ".mts", "0", nil, false)
-        minetest.chat_send_all("Placed trees tile " .. tostring(v))
-        -- place urban third
-        minetest.place_schematic({ x = coord[1], y = startPosition.y, z = coord[2] }, UBCMap.path .. "/schems/ubc_urban_" .. tostring(v) .. ".mts", "0", nil, false)
-        minetest.chat_send_all("Placed urban tile " .. tostring(v))
-        -- place unbreakable map barrier last
-        --minetest.place_schematic({ x = coord[1], y = startPosition.y, z = coord[2] }, UBCMap.path.."/schems/ubc_unbreakable_map_barrier_"..tostring(v)..".mts", "0", nil, false)
-        --minetest.chat_send_all("Placed unbreakable map barrier tile "..tostring(v))
-    end
-end
 
-minetest.register_chatcommand("mosaic_mts", {
-    description = "Mosaic map schematics together for UBC campus",
-    privs = { server = true },
-    func = function(x, y)
-        UBCMap.place()
+        local startPos = { x = coord[1], y = startPosition.y, z = coord[2] }
+        local endPos = { x = startPos.x + 500, y = startPos.y + 500, z = startPos.z + 500 }
+
+        UBCMap:placeSchematic(startPos, endPos, UBCMap.path .. "/schems/ubc_blocks_" .. tostring(v) .. ".mts", "0", nil, false)
+        minetest.chat_send_all("Placed terrain tile " .. tostring(v))
+
+
+        -- place trees second
+        UBCMap:placeSchematic(startPos, endPos, UBCMap.path .. "/schems/ubc_trees_" .. tostring(v) .. ".mts", "0", nil, false)
+        minetest.chat_send_all("Placed trees tile " .. tostring(v))
+
+        -- place urban third
+        UBCMap:placeSchematic(startPos, endPos, UBCMap.path .. "/schems/ubc_urban_" .. tostring(v) .. ".mts", "0", nil, false)
+        minetest.chat_send_all("Placed urban tile " .. tostring(v))
+
+        -- place unbreakable map barrier last
+        if (mc_worldManager == nil) then
+            UBCMap:placeSchematic(startPos, endPos, UBCMap.path .. "/schems/ubc_unbreakable_map_barrier_" .. tostring(v) .. ".mts", "0", nil, false)
+            minetest.chat_send_all("Placed unbreakable map barrier tile " .. tostring(v))
+        end
+
+        UBCMap.storage:set_int("placementProgress", v)
+
+        local completion = tonumber(string.format("%.2f", (v / 28) * 100))
+
+        minetest.chat_send_all(completion .. " % done!")
     end
-})
+
+    UBCMap.storage:set_string("finishedGenerating", "true")
+end
 
 dofile(UBCMap.path .. "/integration.lua")
--- integration.lua
+++ integration.lua
@@ -2,9 +2,16 @@
 if (mc_worldManager) then
     schematicManager.registerSchematicPath("ubc", UBCMap.path .. "/map")
     mc_worldManager.spawnRealmSchematic = "ubc"
+
+    function UBCMap.placeRealm(realm)
+        UBCMap.place(realm.StartPos)
+    end
+
+    local status = UBCMap.storage:get_string("finishedGenerating")
+    if (status == "false" or status == nil) then
+        mc_worldManager.GetSpawnRealm()
+    end
 end
 
-function UBCMap.placeRealm(realm)
-    UBCMap.place(realm.StartPos)
-end
+
 
