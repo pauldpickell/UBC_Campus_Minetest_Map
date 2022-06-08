5c5986bad977200f97901a7a8e443ab44119d996
Lukas Olson <lukas@alumni.ubc.ca>
2022-06-08 14:39:41 -0700

    Forcing new worlds to generate with the UBC map is a bit invasive; but it's an option

--- integration.lua
+++ integration.lua
@@ -1,7 +1,7 @@
 -- If we have Minetest Classroom world manager installed
 if (mc_worldManager) then
     schematicManager.registerSchematicPath("ubc", UBCMap.path .. "/map")
-    mc_worldManager.spawnRealmSchematic = "ubc"
+    --  mc_worldManager.spawnRealmSchematic = "ubc"
 
     function UBCMap.placeRealm(realm)
         UBCMap.place(realm.StartPos)
